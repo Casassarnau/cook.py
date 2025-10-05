0672b5d3694a023b5b26b6a95f33194d261054c4
Arnau Casas Saez <33383702+Casassarnau@users.noreply.github.com>
2025-10-05 14:05:44 +0200

    Added icons and style

--- docs/app.js
+++ docs/app.js
@@ -90,6 +90,11 @@ function recipeApp() {
       return field[this.lang] || field['en'];
     },
 
+    translateFieldToLang(field, lang) {
+      if (typeof field === 'string') return field;
+      return (field && field[lang]) || (field && field['en']);
+    },
+
     filteredCards() {
       // Filter using index items (no translations here, title is a string)
       return this.index.filter(i => {
@@ -137,6 +142,15 @@ function recipeApp() {
       return this.selectedRecipe.ingredientVariations.map(v => this.translateField(v.name));
     },
 
+
+    currentIngredientsAll() {
+      if (this.hasIngredientVariations()) {
+        const v = this.selectedRecipe.ingredientVariations[this.selectedVariationIndex] || this.selectedRecipe.ingredientVariations[0];
+        return v.ingredients || [];
+      }
+      return this.selectedRecipe.ingredients || [];
+    },
+
     currentIngredients() {
       if (this.hasIngredientVariations()) {
         const v = this.selectedRecipe.ingredientVariations[this.selectedVariationIndex] || this.selectedRecipe.ingredientVariations[0];
@@ -171,6 +185,171 @@ function recipeApp() {
           return variationKey && allowed.includes(variationKey);
         })
         .map(e => this.translateField(e.text ?? e));
+    },
+
+    ingredientIcon(text) {
+      const t = String(text || '').toLowerCase();
+      const rules = [
+        // Fruits
+        [/grape|grapes/, '🍇'],
+        [/melon\b|honeydew/, '🍈'],
+        [/watermelon/, '🍉'],
+        [/orange|mandarin|clementine|tangerine/, '🍊'],
+        [/lemon/, '🍋'],
+        [/lime/, '🍋‍🟩'],
+        [/banana/, '🍌'],
+        [/pineapple/, '🍍'],
+        [/mango/, '🥭'],
+        [/\bred\s*apple|apple(?!\s*sauce|\s*cider)/, '🍎'],
+        [/\bgreen\s*apple/, '🍏'],
+        [/pear/, '🍐'],
+        [/peach/, '🍑'],
+        [/cherry|cherries/, '🍒'],
+        [/strawberry|strawberries/, '🍓'],
+        [/blueberry|blueberries/, '🫐'],
+        [/kiwi/, '🥝'],
+        [/tomato/, '🍅'],
+        [/olive|olives/, '🫒'],
+        [/coconut/, '🥥'],
+
+        // Vegetables, legumes, nuts
+        [/avocado/, '🥑'],
+        [/eggplant|aubergine/, '🍆'],
+        [/potato|potatoes/, '🥔'],
+        [/carrot|carrots/, '🥕'],
+        [/corn|maize|sweet\s*corn/, '🌽'],
+        [/pepper\b.*bell|bell\s*pepper|capsicum|pepper\s*(red|green|yellow)/, '🫑'],
+        [/chili|chilli|chile/, '🌶️'],
+        [/cucumber|gherkin/, '🥒'],
+        [/lettuce|greens|leafy\s*greens|cabbage|bok\s*choy|spinach/, '🥬'],
+        [/broccoli/, '🥦'],
+        [/garlic/, '🧄'],
+        [/onion|shallot|spring\s*onion|scallion/, '🧅'],
+        [/peanut|peanuts/, '🥜'],
+        [/bean|beans|lentil|lentils|legume|legumes/, '🫘'],
+        [/chestnut|chestnuts/, '🌰'],
+        [/ginger/, '🫚'],
+        [/pea|peas|snow\s*pea|snap\s*pea|edamame/, '🫛'],
+        [/cashew|cashews|almond|almonds|pistachio|pistachios|walnut|walnuts|hazelnut|hazelnuts/, '🫜'],
+        [/mushroom|porcini|shiitake|button\s*mushroom/, '🍄‍🟫'],
+
+        // Prepared foods
+        [/bread|loaf|toast/, '🍞'],
+        [/croissant/, '🥐'],
+        [/baguette/, '🥖'],
+        [/flatbread|pita|naan|roti|chapati/, '🫓'],
+        [/pretzel/, '🥨'],
+        [/bagel/, '🥯'],
+        [/pancake|pancakes/, '🥞'],
+        [/waffle|waffles/, '🧇'],
+        [/cheese|parmesan|mozzarella|cheddar|gouda|brie|feta|roquefort|gorgonzola|ricotta/, '🧀'],
+        [/ham\b|ribs|pork\s*shoulder/, '🍖'],
+        [/chicken\s*leg|drumstick/, '🍗'],
+        [/beef|steak|sirloin|ribeye/, '🥩'],
+        [/bacon|pancetta/, '🥓'],
+        [/burger|hamburger/, '🍔'],
+        [/fries|chips\b(?!\s*and)/, '🍟'],
+        [/pizza/, '🍕'],
+        [/hot\s*dog/, '🌭'],
+        [/sandwich/, '🥪'],
+        [/taco/, '🌮'],
+        [/burrito/, '🌯'],
+        [/tamale|tamales/, '🫔'],
+        [/kebab|shawarma|gyro|wrap/, '🥙'],
+        [/falafel|meatball|kofta/, '🧆'],
+        [/egg|eggs?\b/, '🥚'],
+        [/fried\s*egg|frying\s*pan/, '🍳'],
+        [/paella|casserole|stew\b.*pan/, '🥘'],
+        [/stew|soup\b(?!\s*stock)|hotpot/, '🍲'],
+        [/fondue|cheese\s*fondue/, '🫕'],
+        [/bowl\s*with\s*spoon|porridge|oatmeal/, '🥣'],
+        [/salad|greens\s*salad/, '🥗'],
+        [/popcorn/, '🍿'],
+        [/butter/, '🧈'],
+        [/\bsalt\b/, '🧂'],
+        [/canned|tin\s*can/, '🥫'],
+        [/pasta|spaghetti|noodles?/, '🍝'],
+
+        // Asian foods
+        [/bento/, '🍱'],
+        [/rice\s*cracker/, '🍘'],
+        [/rice\s*ball|onigiri/, '🍙'],
+        [/cooked\s*rice|steamed\s*rice/, '🍚'],
+        [/curry/, '🍛'],
+        [/ramen|noodle\s*soup|pho|udon|soba|laksa/, '🍜'],
+        [/sweet\s*potato|yakiimo/, '🍠'],
+        [/oden/, '🍢'],
+        [/sushi/, '🍣'],
+        [/shrimp\s*tempura|tempura/, '🍤'],
+        [/fish\s*cake|narutomaki|kamaboko/, '🍥'],
+        [/mooncake/, '🥮'],
+        [/dango/, '🍡'],
+        [/dumpling|gyoza|jiaozi|pierogi|momo/, '🥟'],
+        [/fortune\s*cookie/, '🥠'],
+        [/take\s*out|takeaway|takeout\s*box/, '🥡'],
+
+        // Sweets & desserts
+        [/soft\s*serve|ice\s*cream\s*cone/, '🍦'],
+        [/shaved\s*ice/, '🍧'],
+        [/ice\s*cream(?!\s*cone)/, '🍨'],
+        [/donut|doughnut/, '🍩'],
+        [/cookie|biscuit/, '🍪'],
+        [/birthday\s*cake/, '🎂'],
+        [/cake|shortcake/, '🍰'],
+        [/cupcake/, '🧁'],
+        [/pie/, '🥧'],
+        [/chocolate|cocoa/, '🍫'],
+        [/candy/, '🍬'],
+        [/lollipop/, '🍭'],
+        [/custard|flan|crème\s*caramel|creme\s*caramel/, '🍮'],
+        [/honey/, '🍯'],
+
+        // Drinks & dishware
+        [/baby\s*bottle|formula/, '🍼'],
+        [/milk|cream\b/, '🥛'],
+        [/coffee|espresso|latte|americano|cappuccino/, '☕'],
+        [/teapot/, '🫖'],
+        [/tea|matcha/, '🍵'],
+        [/sake/, '🍶'],
+        [/champagne\s*bottle|sparkling\s*wine/, '🍾'],
+        [/wine/, '🍷'],
+        [/martini|cocktail(?!\s*tropical)/, '🍸'],
+        [/tropical\s*cocktail|piña\s*colada|pina\s*colada/, '🍹'],
+        [/beer\b(?!\s*mug)|lager|ale|stout/, '🍺'],
+        [/beers|clinking\s*beer/, '🍻'],
+        [/clinking\s*glasses|cheers/, '🥂'],
+        [/whiskey|whisky|bourbon|rum|vodka|brandy/, '🥃'],
+        [/pouring|liquid\s*pour/, '🫗'],
+        [/soda|soft\s*drink|cola|cup\s*with\s*straw/, '🥤'],
+        [/bubble\s*tea|boba/, '🧋'],
+        [/juice\s*box/, '🧃'],
+        [/mate/, '🧉'],
+        [/chopsticks/, '🥢'],
+        [/plate|place\s*setting/, '🍽️'],
+        [/fork|knife|cutlery/, '🍴'],
+        [/spoon/, '🥄'],
+        [/chef\s*knife|kitchen\s*knife/, '🔪'],
+        [/jar|mason\s*jar|container/, '🫙'],
+        [/amphora|urn/, '🏺'],
+
+        // Staples & baking basics
+        [/flour|all[-\s]?purpose|ap\s*flour|bread\s*flour|cake\s*flour/, '🌾'],
+        [/yeast/, '🍞'],
+        [/baking\s*powder/, '🎂'],
+        [/baking\s*soda|bicarbonate/, '🧪'],
+        [/(?:brown\s+)?sugar|caster\s*sugar|icing\s*sugar|powdered\s*sugar|confectioners'\s*sugar/, '🍬'],
+        [/vanilla/, '🌼'],
+        [/olive\s*oil/, '🫒'],
+        [/\boil\b|vegetable\s*oil|sunflower\s*oil|canola\s*oil|corn\s*oil|neutral\s*oil/, '🫗'],
+        [/water/, '💧'],
+        [/herb|basil|parsley|cilantro|coriander|thyme|rosemary|oregano|mint|dill|chive|tarragon|sage/, '🌿'],
+        [/rice/, '🍚'],
+        [/fish|salmon|tuna|cod|trout|mackerel|sardine/, '🐟'],
+      ];
+      for (const [regex, icon] of rules) {
+        if (regex.test(t)) return icon;
+      }
+      return '•';
     }
   };
 }
